d8dfb793710d1de00d48a892fc5d3b566c484619
Tadanasi <tadanasi@gmail.com>
2020-10-05 19:50:21 +0300

    Update lab_1.1.js
    Ты пидор, ёпта

--- lab_1.1.js
+++ lab_1.1.js
@@ -1,3 +1,4 @@
+//Lab 1-1
 function random(min, max) {
   return Math.random() * (max - min) + min;
 }
@@ -39,18 +40,35 @@ const s = (array, number) => {
 
 // Функция, систематизирующая вывод всех таблиц по заданому массиву входных данных размером number
 const tabl = (array, number) => {
-  console.log(array); // Вывод массива результатов лабораторной
-  console.log(tablT(array)) // Вывод массива периодов
-  console.log(`Сумма значений периодов для ${number} замеров: ${Math.round((tablT(array).reduce((acc, elem) => acc + Number(elem), 0)) * 10000) / 10000}`);
-  console.log(`Среднее значение периода для ${number} замеров: ${average(array, number)}`);
-  console.log(tablDT(array, average(array, number))); // Вывод массива абсолютных погрешностей
-  let arrayDTpow = tablDT(array, average(array, number)).map(elem => elem = Math.round(elem ** 2 * 100000000) / 100000000); 
-  console.log(arrayDTpow); // Создание и вывод массива квадратов абс. погрешностей
-  console.log(`Сумма квадратов абсолютных погрешностей для ${number} замеров: ${Math.round(arrayDTpow.reduce((acc, elem) => acc + elem, 0) * 100000000) / 100000000}`);
-  console.log(`Среднее значение квадрата абсолютной погрешности для ${number} замеров: ${Math.round((arrayDTpow.reduce((acc, elem) => acc + elem, 0)) / number * 1000000000) / 1000000000}`);
-  console.log(tabln(array, number));
-  console.log(dnN(tabln(array, number), number));
-  console.log(`Случайная погрешность для ${number} замеров: ${s(array, number)}`);
+  let arrayT = tablT(array) //Создание массива периодов
+  let arrayDT = tablDT(array, average(array, number)) //Создание массива абс. погрешностей
+  let arrayDTpow = tablDT(array, average(array, number)).map(elem => elem = Math.round(elem ** 2 * 100000000) / 100000000);  // Создание массива квадратов абс. погрешностей
+  let arrayln = tabln(array, number)
+  let arraydnN = dnN(tabln(array, number), number)
+  console.log(`Таблица для ${number} элементов`)
+  console.log('-------------------------------------------------------------')//Useless line
+  console.log(`| №\t\t|\tT,сек\t|\tПериод\t|\tПогреш.\t|\tКв.погреш.\t|`)
+  for(let i = 0; i<number;i++) {
+    console.log('|-------+-----------+-----------+-----------+---------------|')//Useless line
+    if(arrayDTpow[i]<0.000001  || arrayDTpow[i]===0){console.log(`| ${i+1}.\t|\t${array[i]}\t|\t${arrayT[i]}\t|\t${arrayDT[i]}\t|\t\t${arrayDTpow[i]}\t|`)}
+    else{console.log(`| ${i+1}.\t|\t${array[i]}\t|\t${arrayT[i]}\t|\t${arrayDT[i]}\t|\t${arrayDTpow[i]}\t|`)}
+  }
+  console.log('-------------------------------------------------------------\n')//Useless line
+  
+  console.log(`Сумма значений периодов(Σ T) для ${number} замеров: ${Math.round((tablT(array).reduce((acc, elem) => acc + Number(elem), 0)) * 10000) / 10000}`);
+  console.log(`Среднее значение периода(<T>) для ${number} замеров: ${average(array, number)}\n`);
+  console.log(`Сумма квадратов абсолютных погрешностей(Σ(△ T)²) для ${number} замеров: ${Math.round(arrayDTpow.reduce((acc, elem) => acc + elem, 0) * 100000000) / 100000000}`);
+  console.log(`Среднее значение квадрата абсолютной погрешности(Σ(△ T)² / ${number}) для ${number} замеров: ${Math.round((arrayDTpow.reduce((acc, elem) => acc + elem, 0)) / number * 1000000000) / 1000000000}\n`);
+  console.log(`Таблица интервалов\tТаблица интервалов\nотклонений для ${number}\tотклонений для ${number}\nэлементов\t\t\tэлементов × 1/${number}`)
+  console.log(`-----------------\t---------------------`)//Useless line
+  console.log(`| №\t\t|\t△ T\t|\t| №\t\t|△ T×1/${number}\t|`)
+  for(let k = 0; k<arrayln.length; k++){
+    console.log(`|-------+-------|\t|-------+-----------|`)//Useless line
+    if(arrayln[k]===0) console.log(`| ${k+1}.\t|\t${arrayln[k]}\t|\t| ${k+1}.\t|     ${arraydnN[k]}\t\t|`)
+    else console.log(`| ${k+1}.\t|\t${arrayln[k]}\t|\t| ${k+1}.\t|\t${arraydnN[k]}\t|`)
+  }
+  console.log(`-----------------\t---------------------`)//Useless line
+  console.log(`\nСлучайная погрешность для ${number} замеров: ${s(array, number)}\n`);
 }
 
 // Создание массива количества результатов, попавших в узкие промежутки
